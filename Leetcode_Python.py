0e359b4a114966e2f453aac4fbb4c7d75dbcd45f
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-04-17 13:02:50 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -22,4 +22,30 @@ class Solution:
         for i in range(2,n+1):
             dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
+  
+#746. Min Cost Climbing Stairs
+# Time and Space O(n) for all methods
+# Or space O(1)
+
+#Itrative,space O(1)    
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        res=0
+        dp1,dp2=0,0
+        for i in range(2,len(cost)+1):
+            dp=min(dp1+cost[i-1],dp2+cost[i-2])
+            dp2=dp1
+            dp1=dp
+        return dp
+    
+#Itrative,space O(n)    
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        res=0
+        dp=[0]*(len(cost)+1)
+        dp[0],dp[1]=0,0
+        for i in range(2,len(cost)+1):
+            dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2])
+        return dp[len(cost)]        
+       
         
