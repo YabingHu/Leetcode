ae91f3a86a444eb858841c379d984bdaa3c7a8eb
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-04-17 17:22:42 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -48,4 +48,49 @@ class Solution:
             dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2])
         return dp[len(cost)]        
        
+#303. Range Sum Query - Immutable   
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.dp = nums
+        for i in range(1,len(nums)):
+            self.dp[i]+=self.dp[i-1]
+    def sumRange(self, i: int, j: int) -> int:
+        if i==0:
+            return self.dp[j]
+        else:
+            return self.dp[j]-self.dp[i-1]
+        
+#53. Maximum Subarray    
+
+
+#121. Best Time to Buy and Sell Stock
+#Time O(n),space O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy=float('Inf')
+        res=0
+        for i in range(len(prices)):
+            buy=min(prices[i],buy)
+            res=max(res,prices[i]-buy)
+        return res
+    
+#Divide and Conquer
+#169. Majority Element
+
+#Moore Voting time:O(n) space O(1)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res=0
+        cnt=0
+        for num in nums:
+            if cnt==0:
+                res=num
+                cnt+=1
+            else:
+                if num==res:
+                    cnt+=1
+                else:
+                    cnt-=1
+        return res
+        
         
