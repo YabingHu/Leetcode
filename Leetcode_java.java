8e44c5f296bc94a562fd58d06c95e756f72d508a
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-04-18 17:04:55 -0400

    Update Leetcode_java.java

--- Leetcode_java.java
+++ Leetcode_java.java
@@ -23,3 +23,39 @@ class Solution {
 
 //Linked List
 
+
+//Graph
+//133. Clone Graph
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+//DFS solution, time and space O(n)
+class Solution {
+    public Node cloneGraph(Node node) {
+        HashMap<Node,Node> map=new HashMap<>();
+        return helper(node,map);
+    }
+    public Node helper(Node node,HashMap<Node,Node> map){
+        if(node==null) return null;
+        if(map.containsKey(node)) return map.get(node);
+        Node dup=new Node(node.val,new ArrayList<Node>());
+        map.put(node,dup);
+        for (Node neighbor:node.neighbors){
+            Node clone=helper(neighbor,map);
+            dup.neighbors.add(clone);
+        }
+        return dup;
+    }
+}
+
