e4b2df60958b6e931b77ef275dbf4af4dc95a207
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-04-18 18:25:37 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -219,3 +219,27 @@ class Solution:
                     queue.append(neighbor)
                 dict[t].neighbors.append(dict[neighbor])
         return dup
+
+#200. Number of Islands    
+#
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if len(grid)==0 or len(grid[0])==0: return 0
+        res=0
+        m,n=len(grid),len(grid[0])
+        visited=[[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=='1' and not visited[i][j]:
+                    self.helper(grid,visited,i,j)
+                    res+=1
+        return res
+    def helper(self,grid,visited,x,y):
+        if x<0 or x>=len(grid): return
+        if y<0 or y>=len(grid[0]): return
+        if grid[x][y] !='1' or visited[x][y]: return
+        visited[x][y]=1
+        self.helper(grid,visited,x-1,y)
+        self.helper(grid,visited,x,y-1)
+        self.helper(grid,visited,x,y+1)
+        self.helper(grid,visited,x+1,y)
