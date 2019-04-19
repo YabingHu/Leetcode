c9f2aa20c259e6562b14af84144e523a39b22d80
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-04-19 17:30:32 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -328,3 +328,18 @@ class Solution:
             else:
                 left=mid+1
         return right
+
+#704. Binary Search
+#Time=O(logn), space=O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int: 
+        left,right=0,len(nums)-1
+        while left<=right:
+            mid=left+int((right-left)/2)
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                right=mid-1
+            else:
+                left=mid+1
+        return -1
