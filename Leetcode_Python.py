c92b555a932ffc42eb91d117536657be171a3e60
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-04-19 22:54:31 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -343,3 +343,24 @@ class Solution:
             else:
                 left=mid+1
         return -1
+    
+#33. Search in Rotated Sorted Array
+#Time O(logn), space O(n)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left=0
+        right=len(nums)-1
+        while left<=right:
+            mid=int(left+(right-left)/2)
+            if target==nums[mid]:
+                return mid
+            elif nums[mid]<nums[right]:
+                if nums[mid]<target and nums[right]>=target:
+                    left=mid+1
+                else: right=mid-1
+            else:
+                if nums[left]<=target and nums[mid]> target:
+                    right=mid-1
+                else: left=mid+1
+        return -1
+    
