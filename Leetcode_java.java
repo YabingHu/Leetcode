c18440e569dd6cbcca4ce1a1881a21a3a026f25c
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-04-20 11:08:57 -0400

    Update Leetcode_java.java

--- Leetcode_java.java
+++ Leetcode_java.java
@@ -59,3 +59,19 @@ class Solution {
     }
 }
 
+//Binary Search
+//704. Binary Search
+//Time=O(n),sapce=O(1)
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if (nums[mid]==target) return mid;
+            else if (nums[mid]>target) right=mid-1;
+            else left=mid+1;
+        }
+        return -1;
+    }
+}
