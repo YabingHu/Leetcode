c2d974f6f943eb34767796bdd2dc6221ee1722ea
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-05-05 17:53:48 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -669,6 +669,31 @@ class Solution:
                     dp[i][j]=min(dp[i][j],max(dp[i-1][k],sums[j]-sums[k]))
         return dp[m][n-1]
     
+#4. Median of Two Sorted Arrays
+#O(log(min(n1,n2))),space=O(1)
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n1=len(nums1)
+        n2=len(nums2)
+        if n1>n2:
+            return self. findMedianSortedArrays(nums2,nums1)
+        k=int((n1+n2+1)/2)
+        l=0
+        r=n1
+        while l<r:
+            m1=l+int((r-l)/2)
+            m2=k-m1
+            if nums1[m1]<nums2[m2-1]:
+                l=m1+1
+            else:
+                r=m1
+        m1=l
+        m2=k-l
+        c1=max(-float('Inf') if m1<=0 else nums1[m1-1],-float('Inf') if m2<=0 else nums2[m2-1])
+        if (n1+n2)%2==1:
+            return float(c1)
+        c2=min(float('Inf') if m1>=n1 else nums1[m1],float('Inf') if m2>=n2 else nums2[m2])
+        return (c1+c2)/2.0
 
     
 #270. Closest Binary Search Tree Value
