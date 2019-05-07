bf65d0750b093bd0afc66df02b7dd60d263cd16d
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-05-07 12:22:30 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -1179,4 +1179,26 @@ class Solution:
         print(curr)
         return False
 
+#Array
+#54. Spiral Matrix
+#Time=O(n), space=O(n)
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix: return []
+        R, C = len(matrix), len(matrix[0])
+        seen = [[False] * C for _ in range(R)]
+        ans = []
+        dr = [0, 1, 0, -1]
+        dc = [1, 0, -1, 0]
+        r = c = di = 0
+        for i in range(R * C):
+            ans.append(matrix[r][c])
+            seen[r][c] = True
+            cr, cc = r + dr[di], c + dc[di]
+            if 0 <= cr < R and 0 <= cc < C and seen[cr][cc]==0:
+                r, c = cr, cc
+            else:
+                di = (di + 1) % 4
+                r, c = r + dr[di], c + dc[di]
+        return ans
 
