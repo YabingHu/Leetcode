6d91c1108ca67a0847fb3a687bf61fac8de18d28
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-05-07 23:08:23 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -1292,3 +1292,30 @@ class Solution:
             dict[nums[i]]=i
         return res
     
+#15. 3Sum
+#
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        nums.sort()
+        if len(nums)==0 or nums[0]>0 or nums[-1]<0: return res
+        for i in range(len(nums)):
+            if nums[i]>0:break
+            if i>0 and nums[i]==nums[i-1]:continue
+            target=0-nums[i]
+            left=i+1
+            right=len(nums)-1
+            while left<right:
+                if target>nums[left]+nums[right]:
+                    left+=1
+                elif target<nums[left]+nums[right]:
+                    right-=1 
+                else:
+                    res.append([nums[i],nums[left],nums[right]])
+                    while left<right and nums[left]==nums[left+1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right-1]:
+                        right-=1
+                    left+=1
+                    right-=1
+        return res
