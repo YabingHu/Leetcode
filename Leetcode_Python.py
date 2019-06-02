39712a8d4af61e197a4a9a62b92c67822fb399f5
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-06-02 18:04:33 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -1,4 +1,21 @@
 #Dynamic Programming:
+
+#139. Word Break
+#Time=O(n^2),space=O(n)
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n= len(s)
+        dp= [0] * (n+1)
+        dp[0]=1
+        for i in range (1,n+1):
+            for j in range(i):
+                if dp[j] and (s[j:i] in wordDict):
+                    dp[i]=1
+                    break
+        return bool (dp[n])
+    
+
+
 #Time=O(m*n),space=O(m*n)
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
