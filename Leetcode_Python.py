3f686a3c84e77bef0754e82c3639e79a59f55df3
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-06-03 21:02:31 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -1,4 +1,20 @@
 #Dynamic Programming:
+#312. Burst Balloons
+#Time=O(n^3),space=O(n^2)
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        n=len(nums)
+        nums.insert(0,1)
+        nums.append(1)
+        dp=[[0]*(n+2) for _ in range(n+2)]
+        for l in range(1,n+1):
+            for i in range(1,n-l+2):
+                j=i+l-1
+                for k in range(i,j+1):
+                    dp[i][j]=max(dp[i][k-1]+dp[k+1][j]+nums[i-1]*nums[k]*nums[j+1],dp[i][j])
+        return dp[1][n]
+        
+
 
 #139. Word Break
 #Time=O(n^2),space=O(n)
