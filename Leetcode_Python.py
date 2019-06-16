4595d2b068d1c0b16e5b1d0d73a03fe3cfb48660
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-06-16 11:54:42 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -2550,7 +2550,23 @@ class Solution:
                 stack.pop()
         return not stack
     
-   
+#32. Longest Valid Parentheses
+#Time=O(n), space=O(n)
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        res=0
+        stack=[-1]
+        for i in range(len(s)):
+            if s[i]=='(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    res=max(res,i-stack[-1])
+        return res
+    
 
 
 
