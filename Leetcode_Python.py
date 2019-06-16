d54c924e4f2bc9e8d59b0bea0b622774dc11f6d1
YabingHu <42697916+YabingHu@users.noreply.github.com>
2019-06-16 15:27:52 -0400

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -2567,6 +2567,24 @@ class Solution:
                     res=max(res,i-stack[-1])
         return res
     
+#87. Scramble String
+#Time=3^n, space=3^n
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2) : return False
+        if s1==s2 : return True
+        if sorted(s1) !=sorted(s2): return False
+        for i in range(1, len(s1)):
+            s11=s1[:i]
+            s12=s1[i:]
+            s21=s2[:i]
+            s22=s2[i:]
+            if self.isScramble(s11,s21) and self.isScramble(s12,s22): return True
+            s21=s2[:-i]
+            s22=s2[-i:]
+            if self.isScramble(s11,s22) and self.isScramble(s12,s21): return True
+        return False
+   
 
 
 
